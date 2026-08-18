03be5b581c3a9601366cfd0be54bc7c6afdae2ab
Yabedi <yabedip94@gmail.com>
2026-08-18 17:52:10 +0700

    feat: integrate Allure reporting

--- tests/saucedemo/authentication/successful-login.spec.ts
+++ tests/saucedemo/authentication/successful-login.spec.ts
@@ -1,18 +1,31 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../../src/pages/LoginPage';
 import { USERS } from '../../../src/data/users';
+import { allure } from 'allure-playwright';
 
-test('Successful Login', async ({ page }) => {
-
-    const loginPage = new LoginPage(page);
-    await page.goto('/');
-    await loginPage.login(
-        USERS.standard.username,
-        USERS.standard.password
-    );
-
-    await expect(page).toHaveURL(/inventory\.html/);
+test(
+    'Successful Login',
+    async ({ page }) => {
+        await allure.feature('Authentication');
+        await allure.story('User Login');
+        await allure.epic('Login');
+        await allure.owner('Bedi');
+        await allure.severity('critical');
 
 
-});
+        await allure.description(
+            'Verify that a standard user can successfully log in to SauceDemo.'
+        );
 
+        const loginPage = new LoginPage(page);
+
+        await page.goto('/');
+
+        await loginPage.login(
+            USERS.standard.username,
+            USERS.standard.password
+        );
+
+        await expect(page).toHaveURL(/inventory\.html/);
+    }
+);
